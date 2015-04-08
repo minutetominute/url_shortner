6c786ea4c00f6a5ebe13c9efa9d00b006b40a2ba
Jose-Jeremy <Jose-Jeremy@appacademy.io>
2015-04-08 15:22:54 -0400

    Create Visits Migration

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,6 +3,10 @@ class ShortenedUrl < ActiveRecord::Base
   validates :long_url, presence: true
   validates :submitter_id, presence: true
 
+  belongs_to :submitter,
+    class_name: "User",
+    foreign_key: :submitter_id,
+    primary_key: :id
 
   def self.random_code
     url = SecureRandom.urlsafe_base64[0...16]
@@ -12,11 +16,8 @@ class ShortenedUrl < ActiveRecord::Base
     retry
   end
 
-
   def self.create_for_user_and_long_url!(user, long_url)
     ShortenedUrl.create!(submitter_id: user.id,
     long_url: long_url, short_url: ShortenedUrl.random_code)
   end
-
-
 end
