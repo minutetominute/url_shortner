fb9882721f6a434f8bce104bf83aaace47068cab
Jose-Jeremy <Jose-Jeremy@appacademy.io>
2015-04-08 17:55:53 -0400

    Code review with Tommy

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -14,5 +14,6 @@ class Visit < ActiveRecord::Base
 
   def self.record_visit!(user, shortened_url)
     Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+    # user.visits.create!(shortened_url: shortened_url)
   end
 end
