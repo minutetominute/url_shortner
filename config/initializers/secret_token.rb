becff6580c98e6d03a6199777de02b2f10239f21
Jose-Jeremy <Jose-Jeremy@appacademy.io>
2015-04-08 14:16:53 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = 'a3d76d964b1540353267494e06937cfe97351bdb96541bb29eff3c0d0a16bb789c7b6a66ecacd4b616adad5b1389040f2682b62367aeec520446191cc3ca2b9e'
